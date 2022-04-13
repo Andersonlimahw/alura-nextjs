86a32fa9f6e878eb53e90a1c89dfc532ab327f35
Anderson Lima <andersonlimahw@gmail.com>
2022-04-13 18:15:45 -0300

    🆕 deploy scripts and next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  trailingSlash: true,
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: `/perguntas`,
+        destination: `/faq`, 
+        permanent: true
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
