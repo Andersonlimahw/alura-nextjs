e8fa400472d2d85412c497f459f7f286641386c2
Anderson Lima <andersonlimahw@gmail.com>
2022-04-13 16:21:05 -0300

    🆕 faq page, getStaticProps

--- pages/faq.js
+++ pages/faq.js
@@ -1,3 +1,21 @@
 import FAQScreen from "../src/screens/FAQScreen";
-
 export default FAQScreen;
+
+export async function getStaticProps() {
+    console.log(`Em modo DEV sempre executa a cada acesso.`);
+    console.log(`Roda SOMENTE em tempo de build`);
+
+    const FAQ_API_URL = `https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json`;
+    const faq = await fetch(FAQ_API_URL)
+        .then((response) => 
+            response.json()
+        ).then(data => data);
+
+    return {
+        props: {
+            title: `FAQ Page content`, 
+            date: new Date().toISOString(),
+            faq,
+        }
+    }
+}
